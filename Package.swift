4f7be4ba8b1ce8045bd660542c9e8105d61eb3a2
OHildreth <50595345+OHildreth@users.noreply.github.com>
2023-10-30 22:14:29 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/SwiftVISA/SwiftVISASwift.git", .branch("actor")),
-    .package(url: "https://github.com/HildrethResearchGroup/XPSQ8Kit.git", .branch("main")),
+    .package(url: "https://github.com/HildrethResearchGroup/XPSQ8Kit.git", .branch("master")),
 //    .package(name: "XPSQ8Kit", path: "../XPSQ8Kit"),
     .package(url: "https://github.com/apple/swift-collections", from: "0.0.1")//,
     //.package(url: "https://github.com/armadsen/ORSSerialPort.git", branch: "master"),
